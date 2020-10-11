1ba1e1c41dc4549da59f542cfdf7cb8007161232
puranjay12 <65854124+puranjay12@users.noreply.github.com>
2020-10-11 22:02:57 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# stop-watch
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,83 @@
+var hr, mn, sc;
+var hrAngle, mnAngle, scAngle; 
+var sec,min,hrs, start, stop, reset, simg, stimg, rimg
+var gameState = 0
+
+
+function preload(){
+rimg = loadImage("reset.png")
+simg = loadImage("start.png")
+stimg = loadImage("stop.png")
+}
+
+function setup(){
+    createCanvas(400,400); 
+    //To use angle in Degrees
+    angleMode(DEGREES); 
+    sec=0
+    min=0
+    hrs=0  
+    gameState = 0
+    
+}
+
+function draw(){
+    background(0);
+
+    hr = hour();
+    mn = minute();
+    sc = second();
+
+    if(gameState === 0){
+        start = createSprite(100,300,100,100)
+        start.addImage(simg)
+        start.scale = 0.1
+        stop = createSprite(300,300,100,100)
+        stop.addImage(stimg)
+        stop.scale = 0.1
+        reset = createSprite(215,100,100,10)
+        reset.addImage(rimg)
+        reset.scale = 0.2
+        sec = sec
+    }
+
+    if(gameState === 1){
+        sec = 1304/1304+1+sec
+        
+    }
+
+    
+
+    if(mousePressedOver(start)){
+        gameState = 1
+    }
+
+    if(mousePressedOver(stop)){
+        gameState = 0
+    }
+
+    if(mousePressedOver(reset)){
+        min = 0
+        sec = 0
+        hrs = 0
+    }
+
+    
+    //Calculating time using predefined func from p5.js
+   
+
+    if(sec === 60){
+min = min+1
+sec = 0
+    }
+
+    if(min === 60){
+        hrs = hrs+1
+        min = 0
+            }
+fill(255)
+    //text("time   "+hr+":"+mn+":"+sc,200,200)
+    text(+hrs+":"+min+":"+sec,200,200)
+
+    drawSprites()
+}
